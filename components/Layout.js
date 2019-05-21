7416ceb57106275576e438f715621b070690fbe8
Dan <dan.arias@auth0.com>
2019-05-20 22:07:59 -0400

    Next.js introduction, part 2

--- components/Layout.js
+++ components/Layout.js
@@ -15,8 +15,6 @@ const Layout = props => {
     <div className="Layout">
       <Head>
         <title>WHATABYTE</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
       </Head>
 
       <Header appTitle={appTitle} />
